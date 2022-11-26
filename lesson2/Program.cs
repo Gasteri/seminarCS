3d2dc47f4d400113e25aae4d00bf308a7799c387
Gasteri <tipzov@yandex.ru>
2022-11-26 12:34:25 +0300

    add the square of the number

---
+++ lesson2/Program.cs
@@ -0,0 +1,16 @@
+﻿int number1 = int.Parse(Console.ReadLine());
+
+int number2 = int.Parse(Console.ReadLine());
+
+int c = number1 * number2;
+
+if (number1 == c)
+    {
+        Console.WriteLine("a = b * b");
+    }
+
+else
+
+    {
+        Console.WriteLine("a != b * b");
+    }
