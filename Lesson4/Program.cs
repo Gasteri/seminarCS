de84f7de01305fe3c00ebbb41d96ab4b8dceab6c
Gasteri <tipzov@yandex.ru>
2022-11-26 17:32:34 +0300

    negative numbers

---
+++ Lesson4/Program.cs
@@ -0,0 +1,19 @@
+﻿int integers = int.Parse(Console.ReadLine());
+int integersmin;
+int integersmax;
+if(integers > 0)
+    {
+        integersmin = -integers;
+        integersmax = integers;
+    }
+else
+    {
+       integersmin = integers;
+       integersmax = -integers; 
+    }
+
+while(integersmin <= integersmax)
+    {
+        Console.WriteLine(integersmin);
+        integersmin += 1;
+    }
