dbc4e28a2061a66f165e474afc9903654320d4d4
Gasteri <tipzov@yandex.ru>
2022-11-26 13:30:42 +0300

    add weekday

---
+++ Lesson3/Program.cs
@@ -0,0 +1,30 @@
+﻿int date = int.Parse(Console.ReadLine());
+if(date == 1){
+    Console.WriteLine("Понедельник");
+}
+else
+    if(date == 2){
+    Console.WriteLine("Вторник");
+    }
+    else
+        if(date == 3){
+        Console.WriteLine("Среда");
+        }
+        else
+            if(date == 4){
+            Console.WriteLine("Четверг");
+            }
+             else
+                if(date == 5){
+                Console.WriteLine("Пятница");
+                }
+                else
+                    if(date == 6){
+                    Console.WriteLine("Суббота");
+                    }
+                    else
+                        if(date == 7){
+                        Console.WriteLine("Воскресенье");
+                        }   
+
+
